abc836c28e4525c365d2e77f0ffaa9e394bf66a8
Ritishshelke007 <ritishshelke007@gmail.com>
2024-03-05 23:42:44 +0530

    Updated: Contact validation schema

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import * as Yup from "yup";
 const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const phoneRegEx = /^[0-9]{10}$/;
 
 export const contactSchema = Yup.object({
   name: Yup.string().min(3).max(25).required("Name is required"),
   email: Yup.string().email().required("Email is required"),
-  phone: Yup.string().matches(phoneRegExp,"Phone number is not valid").required("Phone is required"),
+  //   phone: Yup.string().matches(phoneRegExp,"Phone number is not valid").required("Phone is required"),
+  phone: Yup.string()
+    .matches(phoneRegEx, "Phone number must be of 10 digit")
+    .required("Phone is required"),
   message: Yup.string().min(3).required("Message is required"),
 });
