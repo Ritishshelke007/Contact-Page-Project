a58517fc04aa9398d851c4b82d91f21409978d07
Ritishshelke007 <ritishshelke007@gmail.com>
2024-03-05 17:13:05 +0530

    Component added : Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Contact from "./pages/Contact";
 
 function App() {
-
   return (
- <></>
-  )
+    <>
+      <Navbar />
+      <Contact />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import assessliLogo from "../../assets/assessliLogo.png";
+import assessli from "../../assets/assessli2.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-full h-20 bg-slate-50  shadow-md flex justify-between px-10 items-center">
+      <div className="flex justify-center items-center space-x-2">
+        <img src={assessliLogo} alt="logo" className="h-10 w-10" />
+        <img src={assessli} alt="logo" className="w-20" />
+      </div>
+      <div>
+        <ul className="flex justify-center items-center space-x-10 font-semibold ">
+          <li className="cursor-pointer hover:border-b-2 border-black transition duration-300 ease-in">
+            Take Assessment
+          </li>
+          <li className="cursor-pointer hover:border-b-2 border-black ">
+            Solutions
+          </li>
+          <li className="cursor-pointer hover:border-b-2 border-black">
+            About Us
+          </li>
+          <li className="cursor-pointer hover:border-b-2 border-black">
+            Blogs
+          </li>
+          <li className="cursor-pointer hover:border-b-2 border-black">
+            Contact Us
+          </li>
+        </ul>
+      </div>
 
-export default Navbar
+      <div>
+        <button className="bg-slate-900 text-white px-5 py-3 rounded-full">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
